b0166f2028cc81f7ddf67c6ab287b535f6e33574
jalerta <jeff@inverselogic.com>
2016-04-01 22:58:19 -0700

    Remove comments

--- MathGameChapter2/app/src/main/java/com/inverselogic/mathgamechapter2/MainActivity.java
+++ MathGameChapter2/app/src/main/java/com/inverselogic/mathgamechapter2/MainActivity.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.support.v7.widget.Toolbar;
 
-// added comment to force GitHub Desktop commit
 
 public class MainActivity extends Activity {
 
